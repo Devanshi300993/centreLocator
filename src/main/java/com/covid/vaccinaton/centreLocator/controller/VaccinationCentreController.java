828bc1d3b5be34efec2537117f2a38925ad5ed36
Devanshi300993 <devanshi.dhabalia.dd@gmail.com>
2021-04-22 14:30:54 +0530

    removing unnecessary import

--- src/main/java/com/covid/vaccinaton/centreLocator/controller/VaccinationCentreController.java
+++ src/main/java/com/covid/vaccinaton/centreLocator/controller/VaccinationCentreController.java
@@ -5,7 +5,6 @@ import com.covid.vaccinaton.centreLocator.service.VaccinationCentreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
